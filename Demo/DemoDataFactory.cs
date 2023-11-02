a342d3fa7d7ebcd47347b5456419de238dadb216
Chernikov Nikita <14nik20@gmail.com>
2023-11-03 00:24:08 +0200

    Improved code readability

---
+++ Demo/DemoDataFactory.cs
@@ -0,0 +1,44 @@
+﻿using Algorithms.Abstractions;
+using Modelling.Models;
+
+namespace Demo;
+public sealed class DemoDataFactory
+{
+    private readonly ISymmetricKeyGenerator _symmetricKeyGenerator;
+    private readonly IAsymmetricKeysGenerator _asymmetricKeysGenerator;
+
+    public DemoDataFactory(ISymmetricKeyGenerator symmetricKeyGenerator, IAsymmetricKeysGenerator asymmetricKeysGenerator)
+    {
+        _symmetricKeyGenerator = symmetricKeyGenerator;
+        _asymmetricKeysGenerator = asymmetricKeysGenerator;
+    }
+
+    public IReadOnlyList<Candidate> CreateCandidates()
+    {
+        return new List<Candidate>
+        {
+            new Candidate(1, "Ishaan Allison"),
+            new Candidate(2, "Oliver Mendez"),
+            new Candidate(3, "Naomi Winter"),
+        };
+    }
+
+    public IReadOnlyList<Voter> CreateVoters()
+    {
+        return new List<Voter>
+        {
+            new Voter(1, "Jasper Lambert", 17, true, _asymmetricKeysGenerator),     // Too young.
+            new Voter(2, "Jonty Levine", 22, false, _asymmetricKeysGenerator),      // Not capable.
+            new Voter(3, "Nathaniel Middleton", 35, true, _asymmetricKeysGenerator),
+            new Voter(4, "Nathan Bass", 43, true, _asymmetricKeysGenerator),
+            new Voter(5, "Aran Doyle", 21, true, _asymmetricKeysGenerator),
+            new Voter(6, "Julian Harper", 19, true, _asymmetricKeysGenerator),
+            new Voter(7, "Lucian Gross", 20, true, _asymmetricKeysGenerator),
+        };
+    }
+
+    public CentralElectionCommission CreateCentralElectionCommission(IReadOnlyList<Candidate> candidates, IReadOnlyList<Voter> voters)
+    {
+        return new CentralElectionCommission(candidates, voters, _symmetricKeyGenerator);
+    }
+}
---
+++ Demo/ModellingPrinter.cs
@@ -0,0 +1,102 @@
+﻿using Algorithms.Abstractions;
+using FluentResults;
+using Modelling.Extensions;
+using Modelling.Models;
+using Modelling.Models.Ballots;
+using static Demo.UtilityMethods;
+
+namespace Demo;
+public sealed class ModellingPrinter
+{
+    private readonly IEncryptionProvider _encryptionProvider;
+
+    private readonly IAsymmetricKeysGenerator _asymmetricKeysGenerator;
+    private readonly ISignatureProvider _signatureProvider;
+
+    private readonly IObjectToByteArrayTransformer _objectToByteArrayTransformer;
+
+    private readonly IRandomProvider _randomProvider;
+
+    public ModellingPrinter(IEncryptionProvider encryptionProvider, IAsymmetricKeysGenerator asymmetricKeysGenerator, ISignatureProvider signatureProvider, IObjectToByteArrayTransformer objectToByteArrayTransformer, IRandomProvider randomProvider)
+    {
+        _encryptionProvider = encryptionProvider;
+        _asymmetricKeysGenerator = asymmetricKeysGenerator;
+        _signatureProvider = signatureProvider;
+        _objectToByteArrayTransformer = objectToByteArrayTransformer;
+        _randomProvider = randomProvider;
+    }
+
+    public void PrintUsualVoting(CentralElectionCommission commission, IReadOnlyList<Candidate> candidates, IReadOnlyList<Voter> voters)
+    {
+        EncryptedSignedBallot ballot;
+        Result result;
+        foreach (var voter in voters)
+        {
+            var candidateId = _randomProvider.NextItem(candidates).Id;
+            ballot = voter.GenerateBallot(candidateId, commission.BallotEncryptionKey, _signatureProvider, _encryptionProvider, _objectToByteArrayTransformer);
+            result = commission.AcceptBallot(ballot, _signatureProvider, _encryptionProvider, _objectToByteArrayTransformer);
+
+            if (result.IsSuccess)
+            {
+                Console.WriteLine($"{voter.FullName} voted successfully.");
+            }
+            else
+            {
+                PrintError(result);
+            }
+        }
+    }
+
+    public void PrintVotingWithIncorrectBallot(CentralElectionCommission commission)
+    {
+        var ballot = new EncryptedSignedBallot(new byte[] { 4, 6, 8, 0 });
+        var result = commission.AcceptBallot(ballot, _signatureProvider, _encryptionProvider, _objectToByteArrayTransformer);
+
+        if (!result.IsSuccess)
+        {
+            PrintError(result);
+        }
+    }
+
+    public void PrintVotingWithBallotSignedByThirdParty(CentralElectionCommission commission, int candidateId, int voterId)
+    {
+        var keys = _asymmetricKeysGenerator.GenerateKeys();
+
+        var ballot = new Ballot(voterId, candidateId);
+
+        var ballotAsByteArray = _objectToByteArrayTransformer.Transform(ballot);
+        var signedBallot = new SignedBallot(ballot, _signatureProvider.Sign(ballotAsByteArray, keys.PrivateKey), keys.PublicKey);
+
+        var signedBallotAsByteArray = _objectToByteArrayTransformer.Transform(signedBallot);
+        var encryptedSignedBallot = new EncryptedSignedBallot(_encryptionProvider.Encrypt(signedBallotAsByteArray, commission.BallotEncryptionKey));
+
+        var result = commission.AcceptBallot(encryptedSignedBallot, _signatureProvider, _encryptionProvider, _objectToByteArrayTransformer);
+
+        if (!result.IsSuccess)
+        {
+            PrintError(result);
+        }
+    }
+
+    public void PrintVotingWithDoubleBallot(CentralElectionCommission commission, int candidateId, Voter voter)
+    {
+        var ballot = voter.GenerateBallot(candidateId, commission.BallotEncryptionKey, _signatureProvider, _encryptionProvider, _objectToByteArrayTransformer);
+
+        var result = commission.AcceptBallot(ballot, _signatureProvider, _encryptionProvider, _objectToByteArrayTransformer);
+
+        if (!result.IsSuccess)
+        {
+            PrintError(result);
+        }
+    }
+
+    public void PrintVotingResults(CentralElectionCommission commission)
+    {
+        Console.WriteLine("Results:");
+        var results = commission.VotingResults.CandidatesResults.OrderByVotes().ToList();
+        foreach (var candidate in results)
+        {
+            Console.WriteLine($"{candidate.Candidate.FullName} (id: {candidate.Candidate.Id}): {candidate.Votes} votes");
+        }
+    }
+}
