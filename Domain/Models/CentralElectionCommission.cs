6ae642d156a77611c32054b5cee303dfe330811d
Chernikov Nikita <14nik20@gmail.com>
2023-11-02 12:47:19 +0200

    Simplified code and improved cod quality

--- Domain/Models/CentralElectionCommission.cs
+++ Domain/Models/CentralElectionCommission.cs
@@ -11,8 +11,7 @@ public sealed class CentralElectionCommission
 
     private readonly HashSet<int> _acceptedVotersIds = new();
 
-    public bool VotingCompleted { get; set; }
-    private readonly VotingResults _votingResults;
+    public VotingResults VotingResults { get; }
 
     public byte[] BallotEncryptionKey { get; }
 
@@ -29,75 +28,86 @@ public sealed class CentralElectionCommission
         }
 
         BallotEncryptionKey = keyGenerator.GenerateKey();
-        _votingResults = new(_candidates.Values);
+        VotingResults = new(_candidates.Values);
     }
 
     public Result AcceptBallot(EncryptedSignedBallot encryptedSignedBallot, ISignatureProvider signatureProvider, IEncryptionProvider encryptionProvider, IObjectToByteArrayTransformer objectToByteArrayTransformer)
     {
-        return Result
-        .FailIf(VotingCompleted, new Error("Voting is completed."))
-        .Bind(() =>
-            Result.Try(()
-            => objectToByteArrayTransformer.ReverseTransform<SignedBallot>(encryptionProvider.Decrypt(encryptedSignedBallot.EncryptedData, BallotEncryptionKey))
-                ?? throw new InvalidOperationException("Value cannot be transformed to signed ballot."),
-            e => new Error("Message has wrong format or was incorrectly encrypted.").CausedBy(e)))
-        .Bind(sb =>
-        {
-            var signatureIsAuthentic = signatureProvider.Verify(objectToByteArrayTransformer.Transform(sb.Ballot), sb.Signature, sb.PublicKey);
-            if (!signatureIsAuthentic)
-            {
-                return Result.Fail(new Error("The signature is not authentic."));
-            }
-
-            var voterWasFound = _voters.TryGetValue(sb.Ballot.VoterId, out var voter);
-            if (!voterWasFound)
-            {
-                return Result.Fail(new Error("The voter was not found."));
-            }
-
-            var signatureBelongsToVoter = voter!.PublicKey == sb.PublicKey;
-            if (!signatureBelongsToVoter)
-            {
-                return Result.Fail(new Error("The ballot was not signed by the voter."));
-            }
-
-            var voterAbility = voter.IsAbleToVote();
-            if (!voterAbility.IsSuccess)
-            {
-                return voterAbility;
-            }
-
-            return Result.Ok(sb.Ballot);
-        })
-        .Bind(b =>
-        {
-            var voterAlreadyAccepted = _acceptedVotersIds.Contains(b.VoterId);
-            if (voterAlreadyAccepted)
-            {
-                return Result.Fail(new Error("Voter's vote has already been accepted."));
-            }
-
-            var candidateWasFound = _candidates.TryGetValue(b.CandidateId, out var candidateResults);
-            if (!candidateWasFound)
-            {
-                return Result.Fail(new Error("Candidate was not found."));
-            }
-
-            candidateResults!.Votes++;
-            _acceptedVotersIds.Add(b.VoterId);
-
-            return Result.Ok();
-        });
+        return Result.Ok(encryptedSignedBallot)
+        .Bind(esb => DecryptSignedBallot(esb, encryptionProvider, objectToByteArrayTransformer))
+        .Bind(sb => VerifySignature(sb, signatureProvider, objectToByteArrayTransformer))
+        .Bind(sb => VerifyVoter(sb))
+        .Bind(sb => VerifyCandidate(sb).Map(sb => sb.Ballot))
+        .Bind(b => AddVote(b));
     }
 
-    public VotingResults CompleteVoting()
+    private Result<SignedBallot> DecryptSignedBallot(EncryptedSignedBallot encryptedSignedBallot, IEncryptionProvider encryptionProvider, IObjectToByteArrayTransformer objectToByteArrayTransformer)
     {
-        if (!VotingCompleted)
+        return Result.Try(()
+            => objectToByteArrayTransformer.ReverseTransform<SignedBallot>(encryptionProvider.Decrypt(encryptedSignedBallot.EncryptedData, BallotEncryptionKey))
+                ?? throw new InvalidOperationException("Value cannot be transformed to signed ballot."),
+            e => new Error("Message has wrong format or was incorrectly encrypted.").CausedBy(e));
+    }
+
+    private static Result<SignedBallot> VerifySignature(SignedBallot signedBallot, ISignatureProvider signatureProvider, IObjectToByteArrayTransformer objectToByteArrayTransformer)
+    {
+        var signatureIsAuthentic = signatureProvider.Verify(objectToByteArrayTransformer.Transform(signedBallot.Ballot), signedBallot.Signature, signedBallot.PublicKey);
+
+        if (!signatureIsAuthentic)
         {
-            VotingCompleted = true;
-            _votingResults.CompleteVoting();
+            return Result.Fail(new Error("The signature is not authentic."));
         }
 
-        return (VotingResults)_votingResults.Clone();
+        return Result.Ok(signedBallot);
+    }
+
+    private Result<SignedBallot> VerifyVoter(SignedBallot signedBallot)
+    {
+        var voterWasFound = _voters.TryGetValue(signedBallot.Ballot.VoterId, out var voter);
+        if (!voterWasFound)
+        {
+            return Result.Fail(new Error("The voter was not found."));
+        }
+
+        var signatureBelongsToVoter = voter!.PublicKey == signedBallot.PublicKey;
+        if (!signatureBelongsToVoter)
+        {
+            return Result.Fail(new Error("The ballot was not signed by the voter."));
+        }
+
+        var voterAbility = voter.IsAbleToVote();
+        if (!voterAbility.IsSuccess)
+        {
+            return voterAbility;
+        }
+
+        return Result.Ok(signedBallot);
+    }
+
+    private Result<SignedBallot> VerifyCandidate(SignedBallot signedBallot)
+    {
+        var ballot = signedBallot.Ballot;
+
+        var voterAlreadyAccepted = _acceptedVotersIds.Contains(ballot.VoterId);
+        if (voterAlreadyAccepted)
+        {
+            return Result.Fail(new Error("Voter's vote has already been accepted."));
+        }
+
+        var candidateWasFound = _candidates.TryGetValue(ballot.CandidateId, out var candidateResults);
+        if (!candidateWasFound)
+        {
+            return Result.Fail(new Error("Candidate was not found."));
+        }
+
+        return Result.Ok(signedBallot);
+    }
+
+    private Result AddVote(Ballot ballot)
+    {
+        _candidates[ballot.CandidateId]!.Votes++;
+        _acceptedVotersIds.Add(ballot.VoterId);
+
+        return Result.Ok();
     }
 }
--- Domain/Models/VotingResults.cs
+++ Domain/Models/VotingResults.cs
@@ -1,36 +1,10 @@
 ﻿namespace Domain.Models;
-public sealed class VotingResults : ICloneable
+public sealed class VotingResults
 {
     public IReadOnlyCollection<CandidateVotingResults> CandidatesResults { get; }
-    public DateTime? VotingCompletedAt { get; private set; }
 
     public VotingResults(IReadOnlyCollection<CandidateVotingResults> candidateResults)
     {
         CandidatesResults = candidateResults;
-    }
-
-    public void CompleteVoting()
-    {
-        if (VotingCompletedAt is not null)
-        {
-            return;
-        }
-
-        VotingCompletedAt = DateTime.Now;
-    }
-
-    public object Clone()
-    {
-        var candidatesClone = new List<CandidateVotingResults>();
-        foreach (var candidateResults in CandidatesResults)
-        {
-            candidatesClone.Add(new CandidateVotingResults(new Candidate(candidateResults.Candidate.Id, candidateResults.Candidate.FullName)) { Votes = candidateResults.Votes });
-        }
-
-        var clone = new VotingResults(candidatesClone)
-        {
-            VotingCompletedAt = VotingCompletedAt
-        };
-        return clone;
     }
 }
